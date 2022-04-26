9e00a58b63c9642414842e0c5b200035dbc03333
honghao30 <58649794+honghao30@users.noreply.github.com>
2022-04-26 17:49:47 +0900

    파일추가

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader", 'sass-loader'],
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      inject: 'body',
+      minify: {
+        collapseWhitespace: false
+      },       
+      filename: './index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/main.html",
+      inject: 'body',
+      minify: {
+        collapseWhitespace: false
+      },       
+      filename: './main.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "common.css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 8080,
+  },
+};
