1f87fad3770750ae543289679746581d93e0933f
hongsh <hongsh@the-51.com>
2023-08-14 17:25:06 +0900

    채팅 플러스 환경 셋팅

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 // import { hello, add } from "./util";
 import "./style.scss";
-import "./js/ui.js";
-import "./js/scroll.js";
-import "./Swiper.js";
-//import "./js/stand.js";
-//import "./js/scroll_ani";
-import "./js/ui_common.js"
-import "./js/weather_cmp.js";
-import "./js/luckycard.js";
-import "./js/lnb.js";
-import "./js/byte_check.js";
-import "./js/number_check.js";
-import "./js/lunch_menu.js";
-import "./js/video.js";
+import "./scripts/ui.js";
--- webpack.config.js
+++ webpack.config.js
@@ -38,134 +38,13 @@ module.exports = {
       filename: './index.html',
     }),
     new HtmlWebpackPlugin({
-      template: "./src/html/js_calc.html",
+      template: "./src/html/main.html",
       inject: 'body',
       minify: {
         collapseWhitespace: false
       },
-      filename: './html/js_calc.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/js_tab.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/js_tab.html',
-    }),
-
-    new HtmlWebpackPlugin({
-      template: "./src/html/js_gallery_type2.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/js_gallery_type2.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/side-nav.html",
-      //inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/side-nav.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/js_faq.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/js_faq.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/js-gate.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/js-gate.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/gallery01.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/gallery01.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/js_tabtype1.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/js_tabtype1.html',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/html/byte_check.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/byte_check.html',
-    }),    
-    new HtmlWebpackPlugin({
-      template: "./src/html/vechle_number_check.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/vechle_number_check.html',
-    }),     
-    new HtmlWebpackPlugin({
-      template: "./src/html/modal_pupop.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/modal_pupop.html',
-    }),     
-    new HtmlWebpackPlugin({
-      template: "./src/html/modal_pupop2.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/modal_pupop2.html',
-    }),     
-    new HtmlWebpackPlugin({
-      template: "./src/html/dropdown.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/dropdown.html',
-    }),  
-    new HtmlWebpackPlugin({
-      template: "./src/html/dropdown2.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/dropdown2.html',
-    }),     
-    new HtmlWebpackPlugin({
-      template: "./src/html/videocont.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/videocont.html',
-    }),             
-    new HtmlWebpackPlugin({
-      template: "./src/html/lunch_menu.html",
-      inject: 'body',
-      minify: {
-        collapseWhitespace: false
-      },
-      filename: './html/lunch_menu.html',
-    }),
+      filename: './html/main.html',
+    }),   
 
     new MiniCssExtractPlugin({
       filename: "common.css",
